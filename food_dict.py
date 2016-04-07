8e4dc76c4eea83b11f3d71ad878ee67ecf982076
jeni <jeni.gooley.42@gmail.com>
2016-04-06 22:19:53 -0400

    copied/ replaced new code with old code

--- food_dict.py
+++ food_dict.py
@@ -1,25 +1,41 @@
+#!/usr/env/python3
+
 import json
-foods = {}
+
+user_intake = {}
 calories ={'popsicle':45, 'yogurt':120, 'muesli':200, 'beets':70, 'sweet potato':180, 'matcha latte': 230, 'mixed vegetables': 120, 'butter':40 }
 
 
 def calorie_count():						
-    while True:
-        food = input('Enter a food(q for quit):')
-        if food == 'q':
+    while food != q:
+        food = input('Enter a food,  when finished enter q:')
+        
+	if food == 'q':
             break
-        count= int(input('Count:'))
-        foods[food] = count
-    print(" ,".join([food for food in foods]))
+        if food not in calories:
+            add_item = input("That item is not in our database, enter the item name."
+            add_calories = input("Enter the calories in this item.")
+            calories[add_item] = add_calories
+            
 
-    total = 0
-    for food, count in foods.items():
+        else:
+            count= int(input('Number of servings:'))
+            user_intake[food] = count
+            
+     print(" ,".join([food for food in user_intake]))
+     
+    calorie_total  = 0
+    for food, count in user_intake.items():
         if food in calories:
-            total += calories[food] * count 
-    print (total)
-    return foods
+           calorie_total += calories[food] * count 
+            print (calorie_total)
+    return user_intake
     
+    with open('daily.json','w') as daily_file:
+        json.dump(user_intake,daily_file)
+        json.dump(calorie_total, daily_file)
    
-
+if __name__ = "main":
+    calorie_count()
 
 
