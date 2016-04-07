cfd2d374271bd12fd85913e4351f42d1e135a8ee
jeni <jeni.gooley.42@gmail.com>
2016-04-07 11:49:31 -0400

    changed var names, fixed types.

--- food_dict.py
+++ food_dict.py
@@ -3,35 +3,38 @@
 import json
 
 user_intake = {}
-calories ={'popsicle':45, 'yogurt':120, 'muesli':200, 'beets':70, 'sweet potato':180, 'matcha latte': 230, 'mixed vegetables': 120, 'butter':40 }
+caloric_data ={'popsicle':45, 'yogurt':120, 'muesli':200, 'beets':70, 'sweet potato':180, 'matcha latte': 230, 'mixed vegetables': 120, 'butter':40 }
 
 
 def calorie_count():						
-     food = ""
+     food = ''
      while food != 'q':
         
         food = input('Enter a food,  when finished enter q:')
+ 
+        if food == 'q':
+            break
         
-        if food not in calories:
-            add_item = input('That item is not in our database, enter  the item name.')
-            add_calories = input('Enter the calories in this item.')
-            calories[add_item] = add_calories
+        if food not in caloric_data:
+            add_item = input('That item is not in our database, enter  the item name:')
+            add_calories = int(input('Enter the calories in this item:'))
+            caloric_data[add_item] = add_calories
             user_intake[add_item] = add_calories
             
-        else:
+        if food in caloric_data:
             count= int(input('Number of servings:'))
             user_intake[food] = count
         
-        #elif food == 'q':
-            #break
+        
             
         print(" ,".join([food for food in user_intake]))
      
         for food, count in user_intake.items():
             calorie_total  = 0
-            if food in calories:
-               calorie_total += calories[food] * count 
+            if food in caloric_data:
+               calorie_total += caloric_data[food] * count 
                print (calorie_total)
+        
         return user_intake
     
         with open('daily.json','w') as daily_file:
