7d311f165c9307306cf826602c8a1dec3ef6929a
jeni <jeni.gooley.42@gmail.com>
2016-04-06 23:20:25 -0400

    "corrected indents, greyed out break command"

--- food_dict.py
+++ food_dict.py
@@ -7,35 +7,38 @@ calories ={'popsicle':45, 'yogurt':120, 'muesli':200, 'beets':70, 'sweet potato'
 
 
 def calorie_count():						
-    while food != q:
+     food = ""
+     while food != 'q':
+        
         food = input('Enter a food,  when finished enter q:')
         
-	if food == 'q':
-            break
         if food not in calories:
-            add_item = input("That item is not in our database, enter the item name."
-            add_calories = input("Enter the calories in this item.")
+            add_item = input('That item is not in our database, enter  the item name.')
+            add_calories = input('Enter the calories in this item.')
             calories[add_item] = add_calories
+            user_intake[add_item] = add_calories
             
-
         else:
             count= int(input('Number of servings:'))
             user_intake[food] = count
+        
+        #elif food == 'q':
+            #break
             
-     print(" ,".join([food for food in user_intake]))
+        print(" ,".join([food for food in user_intake]))
      
-    calorie_total  = 0
-    for food, count in user_intake.items():
-        if food in calories:
-           calorie_total += calories[food] * count 
-            print (calorie_total)
-    return user_intake
+        for food, count in user_intake.items():
+            calorie_total  = 0
+            if food in calories:
+               calorie_total += calories[food] * count 
+               print (calorie_total)
+        return user_intake
     
-    with open('daily.json','w') as daily_file:
-        json.dump(user_intake,daily_file)
-        json.dump(calorie_total, daily_file)
+        with open('daily.json','w') as daily_file:
+            json.dump(user_intake,daily_file)
+            json.dump(calorie_total, daily_file)
    
-if __name__ = "main":
-    calorie_count()
+#if __name__ == "main":
+calorie_count()
 
 
