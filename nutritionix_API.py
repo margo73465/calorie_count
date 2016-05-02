982a7764e3e5e04219ddd6700f053584b18ac765
Margo <margo.lee.smith@gmail.com>
2016-05-02 14:40:52 -0400

    change fields parameter to comma separated string

--- nutritionix_API.py
+++ nutritionix_API.py
@@ -1,7 +1,7 @@
 import json
 import requests
 
-payload_s = {'fields':['item_name','2Citem_id','2Cbrand_name','2Cnf_calories', '2Cnftotal_fat'],'appId':'7f0a2caa','appKey':'65f3c1d9a1d3c6f7edcf2802023fe069'}
+payload_s = {'fields': 'item_name,item_id,brand_name,nf_calories,nftotal_fat','appId':'7f0a2caa','appKey':'65f3c1d9a1d3c6f7edcf2802023fe069'}
 r_search = requests.get('https://api.nutritionix.com/v1_1/search/beets', params=payload_s)
 r_search = r_search.json()
 print(json.dumps(r_search, indent=4, sort_keys=True))
